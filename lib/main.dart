0b091ce19142827359798d15f76f3fa8fe2a26b2
Chaiwiwat <melondev@icloud.com>
2022-12-31 02:20:50 +0700

    เพิ่มหน้า HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,59 +14,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme =
+        ThemeData(colorSchemeSeed: Colors.lightGreen, useMaterial3: true);
+
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+          colorScheme: theme.colorScheme,
+          useMaterial3: theme.useMaterial3,
+          splashColor: Colors.transparent,
+          textTheme: GoogleFonts.itimTextTheme(theme.textTheme),
+          navigationBarTheme: NavigationBarThemeData(
+            //backgroundColor: Color(0xff0100f5b),
+            //indicatorColor: Color(0xff5454c2),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+            labelTextStyle: MaterialStateProperty.all(TextStyle(
+              color: theme.colorScheme.onPrimaryContainer,
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+            )),
+            iconTheme: MaterialStateProperty.all(IconThemeData(
+              color: theme.colorScheme.onPrimaryContainer,
+              size: 25,
+            )),
+          )),
+      home: const HomePage(),
     );
   }
 }
